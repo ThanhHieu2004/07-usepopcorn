f39884f5054d7f1b766b49f4857e44345a30755c
Thanh Hieu <thanhhieu8304@gmail.com>
2025-09-03 09:57:30 +0700

    Enhance StarRating component with dynamic rating state and color customization

--- src/index.js
+++ src/index.js
@@ -7,6 +7,6 @@ import StarRating from "./StarRating";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* <App /> */ <StarRating maxRating={10} />}
+    {/* <App /> */ <StarRating maxRating={10} color="#bcbc13cc" />}
   </React.StrictMode>
 );
